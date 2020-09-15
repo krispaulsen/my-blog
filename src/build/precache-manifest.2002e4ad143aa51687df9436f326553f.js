8b1f54eccb2241bc8b3d3a76a7d25cb897883a8d
Kris Paulsen <kpaulsen@columbia.com>
2020-09-15 10:09:27 -0700

    First Commit

---
+++ src/build/precache-manifest.2002e4ad143aa51687df9436f326553f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cf07105d40670ae88b741a97da359c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c32a6ec1cbc5f986217",
+    "url": "/static/css/main.3ebbcb97.chunk.css"
+  },
+  {
+    "revision": "79294f2972c18d11563d",
+    "url": "/static/js/2.637d709d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.637d709d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c32a6ec1cbc5f986217",
+    "url": "/static/js/main.6689c590.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
